af3d03125235ebb5f84247cbd33ad9826017c106
Armando Tavares <armandotavares6@gmail.com>
2017-11-10 08:33:39 -0700

    ground zero

--- DemoApp/scripts/web-server.js
+++ DemoApp/scripts/web-server.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var rootPath = path.normalize(__dirname + '/../');
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const rootPath = path.normalize(__dirname + '/../');
 
 app.use(express.static(rootPath + '/app'));
 
 app.listen(8000);
 console.log("Listening on port 8000...");
+
+const someName = function(params) {
+    
+};
